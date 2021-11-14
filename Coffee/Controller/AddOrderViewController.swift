42fc153f090abc4d863b8477aca6e435c7e1ca1a
Michael <mrflowers22@uwalumni.com>
2021-11-14 09:41:49 -0600

    implemented didSelectRow and didDeselectRow to allow only one selection to be made

--- Coffee/Controller/AddOrderViewController.swift
+++ Coffee/Controller/AddOrderViewController.swift
@@ -41,14 +41,11 @@ class AddOrderViewController: UIViewController, UITableViewDelegate, UITableView
         return cell
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none 
+    }
 }
