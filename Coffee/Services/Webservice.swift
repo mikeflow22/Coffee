b111a950c4cf4b72fe20e04a3069aed9a63d82be
Michael <mrflowers22@uwalumni.com>
2021-11-13 19:18:39 -0600

    created Webserivce file, NetworkError enum, and Resource struct

---
+++ Coffee/Services/Webservice.swift
@@ -0,0 +1,38 @@
+//
+//  Webservice.swift
+//  Coffee
+//
+//  Created by Michael Flowers on 11/13/21.
+//
+
+import Foundation
+enum NetworkError: Error {
+    case decodingError
+    case domainError
+    case urlError
+}
+struct Resource<T: Codable> {
+    let url: URL
+}
+
+class Webservice {
+    func load<T>(resource: Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        
+        URLSession.shared.dataTask(with: resource.url) { data , response , error in
+            guard let data = data, error == nil else {
+                completion(.failure(.domainError))
+                return
+            }
+            let result = try? JSONDecoder().decode(T.self, from: data)
+            if let result = result {
+                //passing data to ui so it has to be done on the main thread
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            } else {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+        
+    }
+}
